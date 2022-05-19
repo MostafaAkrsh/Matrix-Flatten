cc8dd1c75654a231c5f9092b37832b15cc735bd9
MostafaAkrsh <mostafasamir355@gmail.com>
2022-05-19 23:29:30 +0200

    enhancing the usability of the code

--- flat.py
+++ flat.py
@@ -1,8 +1,14 @@
 import numpy as np
 
-def flat_array(array_3D):
-    return array_3D.reshape(-1)
-
+def flat_matrix(matrix_3D):
+    """
+    Flatten matrix function
+    Parameters:
+        matrix_3D (numpy array)
+    Returns:
+        array_1D (numpy array)
+    """
+    return matrix_3D.reshape(-1)
 
 
 arr = np.array([
@@ -13,4 +19,27 @@ arr = np.array([
                  [7, 8]] 
                  ])
 
-print(flat_array(arr))
+# Prepare matrix by Input 
+print("Enter Dimensions of 3D Matrix:")
+n = int(input("Enter N:"))
+m = int(input("Enter M:"))
+p = int(input("Enter P:"))
+
+#initalize a 3D matrix by the input parameters
+np.zeros([n,m,p])
+print("Enter the entries in a single line (separated by space): ")
+
+#slice the input and map it into list
+entries = list(map(int, input().split()))
+
+#put the the list in the np.array
+matrix = np.array(entries).reshape(n,m,p)
+print("Before calling the function")
+print(matrix)
+
+
+flatten = flat_matrix(matrix)
+
+#print the output
+print("After calling the function")
+print(flatten)
